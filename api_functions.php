259263f9b2a4bcff8c4f4efefa6754df7d8fce6a
eltha9 <theadosto@gmail.com>
2020-01-15 20:40:56 +0100

    remove a problem

--- api_functions.php
+++ api_functions.php
@@ -1,7 +1,7 @@
 <?php
 
 // $curl = curl_init();
-// curl_setopt($curl, CURLOPT_URL, 'http://api.openweathermap.org/data/2.5/weather?q=Paris&APPID=9e8150c9d6fbf87d678d2cf7f7a2c00a');
+// curl_setopt($curl, CURLOPT_URL, link');
 // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 // $result = curl_exec($curl);
 // // curl_close($curl);
